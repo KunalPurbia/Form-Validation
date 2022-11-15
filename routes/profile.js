d4f9530aa091a3205523752e31de891a6beb038b
KunalPurbia <Kunal.Purbia@softobiz.com>
2022-11-15 12:37:34 +0530

    Added user email in cookie after login

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var path = require("path")
-
-var publicDir = path.join(__dirname, '..', 'public')
+const mongoose = require('mongoose');
+const User = mongoose.model("user");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("Hello");
 });
+
+router.post('/update', function(req, res){
+  res.send("Update");
+})
+
+router.post('/delete', function(req, res){
+  res.send("Delete");
+})
 
 module.exports = router;
